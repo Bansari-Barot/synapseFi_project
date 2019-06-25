4a0bfc2362820eba4ebe7091b960c20a9786d72d
Bansari Barot <bbarot@horizon.csueastbay.edu>
2019-06-25 14:05:42 -0700

    initial create user and project structure

---
+++ webapp/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config(object):
+
+    MONGO_URI = "mongodb://localhost:27017/mercurydb"
+    CLIENT_ID="lient_id_thqmPge9O0SYoCV0syEbR8u6WKJHaUABN4X5kf3d"
+    CLIENT_SECURE="client_secret_D4WXQYE21cz78JamhHnybrlTMS90V3o0pK6GRqtv"
